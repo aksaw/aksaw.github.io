93d6b2f3b6eb63bbed122d13d9c9a6804837d3cf
akshaysawhney1 <akshaysawhney1@gmail.com>
2017-07-16 03:36:08 -0400

    color changes + added text

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
-var scene, camera, renderer, controls;
+var scene, camera, renderer;
 var geometry, material, mesh;
+// var controls;
 
 var mouseX = 0, mouseY = 0;
 var windowHalfX = window.innerWidth / 2;
 var windowHalfY = window.innerHeight / 2;
-
-document.addEventListener( 'mousemove', onDocumentMouseMove, false );
 
 init();
 animate();
@@ -16,8 +15,8 @@ function init() {
     camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
     camera.position.z = 1000;
 
-    geometry = new THREE.IcosahedronGeometry( 350, 1 );
-    material = new THREE.MeshBasicMaterial( { color: 0x888888, wireframe: true } );
+    geometry = new THREE.IcosahedronGeometry( 400, 1 );
+    material = new THREE.MeshBasicMaterial( { color: 0xAAAAAA, wireframe: true } );
 
     mesh = new THREE.Mesh( geometry, material );
     scene.add( mesh );
@@ -25,12 +24,15 @@ function init() {
     renderer = new THREE.WebGLRenderer({ alpha: true });
     renderer.setSize( window.innerWidth, window.innerHeight );
 
-    controls = new THREE.OrbitControls( camera, renderer.domElement );
-    controls.enableDamping = true;
-    controls.dampingFactor = 0.25;
-    controls.enableZoom = false;
+    // controls = new THREE.OrbitControls( camera, renderer.domElement );
+    // controls.enableDamping = true;
+    // controls.dampingFactor = 0.25;
+    // controls.enableZoom = false;
 
     document.body.appendChild( renderer.domElement );
+    
+    document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+    window.addEventListener( 'resize', onWindowResize, false );
 }
 
 
@@ -39,11 +41,19 @@ function onDocumentMouseMove(event) {
     mouseY = ( event.clientY - windowHalfY ) * 1;
 }
 
+function onWindowResize() {
+    windowHalfX = window.innerWidth / 2;
+    windowHalfY = window.innerHeight / 2;
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+}
+
 
 function animate() {
     requestAnimationFrame( animate );
     
-    controls.update();
+    // controls.update();
 
     mesh.rotation.x += 0.005;
     mesh.rotation.y += 0.010;
@@ -57,8 +67,8 @@ function render() {
     // ry = Math.sin( time * 0.3 ) * 0.5,
     // rz = Math.sin( time * 0.2 ) * 0.5;
     
-    camera.position.x += ( mouseX - camera.position.x ) * .05;
-    camera.position.y += ( - mouseY - camera.position.y ) * .05;
+    camera.position.x += ( mouseX - camera.position.x ) * .055;
+    camera.position.y += ( - mouseY - camera.position.y ) * .055;
     camera.lookAt( scene.position );
     
     // group.rotation.x = rx;
